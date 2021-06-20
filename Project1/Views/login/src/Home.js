7de31be61419ac1d6c0cd102ca0c4bd2c80d0e68
Ata Atasoy <h.ataatasoy@gmail.com>
2021-06-20 20:43:34 +0300

    Add signout button

--- Project1/Views/login/src/Home.js
+++ Project1/Views/login/src/Home.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Home(){
 
     const { state } = useLocation();
+    const navigate = useNavigate();
+
     console.log(state);
 
+    function handleOnClick(e){
+        e.preventDefault();
+        navigate("/");
+    }
+
     return (
-        <h1>Welcome {state}!</h1>
+        <div class="Homepage">
+            <h1>Welcome {state}!</h1>
+            <button onClick={handleOnClick}>Sign Out</button>
+        </div>
     );
 }
