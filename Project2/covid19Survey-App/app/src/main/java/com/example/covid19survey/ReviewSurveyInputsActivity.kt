2805ba4ea86c4ca66e7c11e9ab8a1d01f2f3ff03
Ata Atasoy <h.ataatasoy@gmail.com>
2021-07-01 15:44:02 +0300

    add success message upon submitting the survey

--- Project2/covid19Survey-App/app/src/main/java/com/example/covid19survey/ReviewSurveyInputsActivity.kt
+++ Project2/covid19Survey-App/app/src/main/java/com/example/covid19survey/ReviewSurveyInputsActivity.kt
@@ -1,33 +1,48 @@
 package com.example.covid19survey
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.ViewGroup
 import android.widget.*
 
 class ReviewSurveyInputsActivity : AppCompatActivity() {
 
     private lateinit var listView: ListView
+    private lateinit var submitButton: Button
+    private var message: String = "Survey Submitted Successfully!"
+    private val duration = Toast.LENGTH_LONG
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review_survey_inputs)
 
         listView = findViewById<ListView>(R.id.reviewSurveyInputsView)
+        submitButton = findViewById(R.id.submitSurveyButton)
 
         // Survey result is in this comma separated string
-        val message = intent.getStringExtra(EXTRA_MESSAGE)
-        val surveyResults: Array<String>
-        if (message != null) {
-            surveyResults = message.split(",").toTypedArray()
-            val adapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, surveyResults)
-            listView.adapter = adapter
-        }
-
         val surveyResult = intent.getStringExtra(EXTRA_MESSAGE)
         Log.i(surveyResult, "Result of the survey: $surveyResult")
 
+        val surveyResults: Array<String>
+        if (surveyResult != null) {
+            surveyResults = surveyResult.split(",").toTypedArray()
+            val adapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1,
+                surveyResults)
+            listView.adapter = adapter
+        }
 
+        submitButton.setOnClickListener {
+            // Display success message
+            val toast = Toast.makeText(applicationContext, message, duration)
+            toast.show()
+
+            // Redirect to starting point
+            val intent = Intent(this, MainActivity::class.java).apply {
+                putExtra(EXTRA_MESSAGE, "")
+            }
+            startActivity(intent)
+        }
 
     }
 }
