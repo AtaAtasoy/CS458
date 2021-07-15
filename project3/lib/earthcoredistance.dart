ee6a7779f5c4a3db6ca4f46298e8b43a59167799
Ata Atasoy <h.ataatasoy@gmail.com>
2021-07-15 21:54:33 +0300

    refactor code

--- project3/lib/earthcoredistance.dart
+++ project3/lib/earthcoredistance.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class EarthCoreDistance extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text("Calculate Distance to the Earth's Core");
+    return Text("Calculate Distance to the Earth's Core", textDirection: TextDirection.ltr);
   }
 }
