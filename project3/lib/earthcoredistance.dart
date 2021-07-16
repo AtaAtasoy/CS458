2f465b0ddd1accc8b2a0f62a42a7701d67b21e7f
Ata Atasoy <h.ataatasoy@gmail.com>
2021-07-17 00:43:19 +0300

    Greenline 3rd test case

--- project3/lib/earthcoredistance.dart
+++ project3/lib/earthcoredistance.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 class EarthCoreDistance extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text("Calculate Distance to the Earth's Core", textDirection: TextDirection.ltr);
+    return Column(children: [
+      Text("Calculate Distance to the Earth's Core"),
+      ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(primary: Colors.blue),
+          child: Row(children: [
+            Icon(
+              Icons.map_rounded,
+              key: Key('icon-go'),
+            ),
+            Text('Find my distance!')
+          ])),
+    ]);
   }
 }
