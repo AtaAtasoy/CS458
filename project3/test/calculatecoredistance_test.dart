8f517bb994bef6edd3d21ac41c7db7f0c9fb8f5b
Ata Atasoy <h.ataatasoy@gmail.com>
2021-07-17 13:44:23 +0300

    update test case for geolocator

--- project3/test/calculatecoredistance_test.dart
+++ project3/test/calculatecoredistance_test.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:project3/coredistancecalculator.dart';
 
 void main() {
-  test("testing geolocator", () {
-    Future<double> latitude = calculateDistanceToEarthCore("args");
-    expect(latitude, completion(equals(calculateDistanceToEarthCore("args"))));
+  test("testing geolocator", () async {
+    Position pos = await calculateDistanceToEarthCore();
+    expect(pos, completion(equals(Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high))));
   });
 }
