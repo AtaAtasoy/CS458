f1c4492b9771c74e1e76c9af5640b75d98e43f83
Ata Atasoy <h.ataatasoy@gmail.com>
2021-07-17 12:58:49 +0300

    update redline code

--- project3/test/calculatecoredistance_test.dart
+++ project3/test/calculatecoredistance_test.dart
@@ -6,11 +6,17 @@ void main() {
   testWidgets('Calculate distance button callback test',
       (WidgetTester tester) async {
     var distance = 0;
-    final testWidget = MaterialApp(home: EarthCoreDistance(
+    final testWidget = MaterialApp(
+        home: EarthCoreDistance(
       onPressed: () => distance = distance + 1,
     ));
 
     await tester.pumpWidget(testWidget);
     await tester.pumpAndSettle();
+
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pumpAndSettle();
+
+    expect(distance, distance + 1);
   });
 }
