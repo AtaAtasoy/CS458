ca6e084baa8f48003c8fbbac33891a68763100d9
Ata Atasoy <h.ataatasoy@gmail.com>
2021-07-17 23:01:32 +0300

    redline invalidCoordinateInput test

--- project_3_map/test/coordinateInput_test.dart
+++ project_3_map/test/coordinateInput_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:project_3_map/screens/manualCore/earthCoreDistance.dart';
+import 'package:quiver/testing/src/async/fake_async.dart';
 
 void main() {
   testWidgets('validate coordinate inputs', (WidgetTester tester) async {
@@ -12,6 +13,8 @@ void main() {
     await tester.pumpAndSettle();
 
     await tester.enterText(find.byKey(Key('latitude-field')), "hi");
+    await tester.tap(find.byType(ElevatedButton));
+
     expect(find.text("Invalid Input"), findsOneWidget);
   });
 }
