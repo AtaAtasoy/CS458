a4e0a0a1c66505883ecb35b38241d3dbeb358d2b
Ata Atasoy <h.ataatasoy@gmail.com>
2021-07-17 13:32:11 +0300

    Redline geolocator test

--- project3/test/calculatecoredistance_test.dart
+++ project3/test/calculatecoredistance_test.dart
@@ -1,22 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:project3/earthcoredistance.dart';
+import 'package:project3/coredistancecalculator.dart';
 
 void main() {
-  testWidgets('Calculate distance button callback test',
-      (WidgetTester tester) async {
-    var distance = 0;
-    final testWidget = MaterialApp(
-        home: EarthCoreDistance(
-      onPressed: () => distance = distance + 1,
-    ));
-
-    await tester.pumpWidget(testWidget);
-    await tester.pumpAndSettle();
-
-    await tester.tap(find.byType(ElevatedButton));
-    await tester.pumpAndSettle();
-
-    expect(distance, 1);
+  test("testing geolocator", () {
+    Future<double> latitude = calculateDistanceToEarthCore("args");
+    expect(latitude, completion(equals(calculateDistanceToEarthCore("args"))));
   });
 }
