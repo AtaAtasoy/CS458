b4946b603f992bf94c68ba7a02a3afe63655a301
Ata Atasoy <h.ataatasoy@gmail.com>
2021-07-17 23:10:08 +0300

    Redline automaticGPS location finder

--- project_3_map/test/coordinateInput_test.dart
+++ project_3_map/test/coordinateInput_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:project_3_map/screens/manualCore/earthCoreDistance.dart';
 
 void main() {
-  testWidgets('validate coordinate inputs', (WidgetTester tester) async {
+  testWidgets('alphabetic coordinate inputs', (WidgetTester tester) async {
     final testWidget = MaterialApp(
       home: EarthCoreDistance(),
     );
@@ -16,5 +16,43 @@ void main() {
     await tester.pump(new Duration(seconds: 5));
 
     expect(find.text("Invalid Input"), findsOneWidget);
+  });
+  test2();
+  test3();
+}
+
+void test2() {
+  testWidgets('special character coordinate inputs',
+      (WidgetTester tester) async {
+    final testWidget = MaterialApp(
+      home: EarthCoreDistance(),
+    );
+
+    await tester.pumpWidget(testWidget);
+    await tester.pumpAndSettle();
+
+    await tester.enterText(find.byKey(Key('latitude-field')), "?-.,");
+    await tester.enterText(find.byKey(Key('longtitude-field')), "?-.,~");
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pump(new Duration(seconds: 5));
+
+    expect(find.text("Invalid Input"), findsOneWidget);
+  });
+}
+
+void test3() {
+  testWidgets('special character coordinate inputs',
+      (WidgetTester tester) async {
+    final testWidget = MaterialApp(
+      home: EarthCoreDistance(),
+    );
+
+    await tester.pumpWidget(testWidget);
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pump(new Duration(seconds: 5));
+
+    expect(find.textContaining("LAT: "), findsOneWidget);
   });
 }
