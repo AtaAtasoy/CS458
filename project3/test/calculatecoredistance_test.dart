b5a67fbeb378e4804be04a4d7f862415413c342f
Ata Atasoy <h.ataatasoy@gmail.com>
2021-07-17 12:57:48 +0300

    Redline onPressed listener for the button

---
+++ project3/test/calculatecoredistance_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:project3/earthcoredistance.dart';
+
+void main() {
+  testWidgets('Calculate distance button callback test',
+      (WidgetTester tester) async {
+    var distance = 0;
+    final testWidget = MaterialApp(home: EarthCoreDistance(
+      onPressed: () => distance = distance + 1,
+    ));
+
+    await tester.pumpWidget(testWidget);
+    await tester.pumpAndSettle();
+  });
+}
