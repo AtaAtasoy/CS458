91b83ed1c06a9a6143b021745faadd53eae29b4d
Ata Atasoy <h.ataatasoy@gmail.com>
2021-07-17 12:39:40 +0300

    Greenline 4th case

--- project3/lib/earthcoredistance.dart
+++ project3/lib/earthcoredistance.dart
@@ -1,20 +1,31 @@
+
 import 'package:flutter/material.dart';
 
 class EarthCoreDistance extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Text("Calculate Distance to the Earth's Core", textDirection: TextDirection.ltr),
-      ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(primary: Colors.blue),
-          child: Row(children: [
-            Icon(
-              Icons.map_rounded,
-              key: Key('icon-go'),
-            ),
-            Text('Find my distance!', textDirection: TextDirection.ltr)
-          ])),
-    ]);
+    return MaterialApp(
+      title: 'EarthCoreDistance',
+      home: Scaffold(
+            body: Column(children: [
+              Text("Calculate Distance to the Earth's Core"),
+              TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter your coordinates')),
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(primary: Colors.blue),
+                  child: Row(children: [
+                    Icon(
+                      Icons.map_rounded,
+                      key: Key('icon-go'),
+                    ),
+                    Text("Find my distance!")
+                  ]))
+            ]),
+          ),
+    );
   }
 }
