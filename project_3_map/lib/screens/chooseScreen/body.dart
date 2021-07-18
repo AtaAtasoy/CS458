ff68822e09ff1dd7962588177f2711ee074adf31
Ata Atasoy <h.ataatasoy@gmail.com>
2021-07-18 20:44:00 +0300

    refactor code

--- project_3_map/lib/screens/chooseScreen/body.dart
+++ project_3_map/lib/screens/chooseScreen/body.dart
@@ -4,69 +4,45 @@ import 'package:project_3_map/screens/manual/manualScreen.dart';
 import 'package:project_3_map/screens/manualCore/earthCoreDistance.dart';
 
 class Body extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 100.0),
-      
-      child: Column(
-        children: [
-
-          SizedBox(height: 100.0,),
-
-          Text(
-                  "Choose the option you want to use",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.black,
-                  
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-          
-          SizedBox(height: 130.0,),
-
-      
-
-          TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, ManualScreen.routeName);
-              },
-              child: Text("Show City",
-              style: TextStyle(
-                color: Colors.blue
-              )),
-              ),
-
-          
-          TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, AutomaticScreen.routeName);},
-              child: Text("Distance to Big Bang",
-              style: TextStyle(
-                color: Colors.blue
-              )),
-              ),
-
-              TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, EarthCoreDistance.routeName);},
-              child: Text("Distance to Core",
-              style: TextStyle(
-                color: Colors.blue
-              )),
-              ),
-
-        
-
-              
-
-                  
-        ]
-        
-        
-      ),
+      child: Column(children: [
+        SizedBox(
+          height: 100.0,
+        ),
+        Text(
+          "Choose the option you want to use",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 130.0,
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.pushNamed(context, ManualScreen.routeName);
+          },
+          child: Text("Show City", style: TextStyle(color: Colors.blue)),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.pushNamed(context, AutomaticScreen.routeName);
+          },
+          child: Text("Distance to the Big Ben",
+              style: TextStyle(color: Colors.blue)),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.pushNamed(context, EarthCoreDistance.routeName);
+          },
+          child: Text("Distance to Core", style: TextStyle(color: Colors.blue)),
+        ),
+      ]),
     );
   }
 }
